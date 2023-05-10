b2c4e60e53fede5cdcb17bef5399e7a9c426ca3e
joshschaerer <joshua.schaerer@students.fhnw.ch>
2023-05-10 14:09:49 +0200

    added page transition animations to the landing page
    - updated the existing fadeIn animations for the landing page
    - created new fadeOut animations on page transitions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,36 @@
+/*=============== GENERAL ===============*/
+// Access the body element
+const body = document.body;
+
+// Check the motion preference
+const prefersReducedMotion =
+  !window.matchMedia("(prefers-reduced-motion: reduce)") ||
+  window.matchMedia("(prefers-reduced-motion: reduce)").matches;
+
+/*=============== HEADER ===============*/
+// Access the header element
+const header = document.getElementById("header");
+
+/*===== TOGGLE PAGE ANIMATION =====*/
+// Validate whether the header element exists
+// and the motion preference is not reduced
+if (header && !prefersReducedMotion) {
+  // Animation before leaving the page
+  header.querySelectorAll(".nav__link").forEach((link) => {
+    // Listen for a mouse click event
+    link.addEventListener("click", (e) => {
+      // Toggle the animation
+      body.classList.add("unload-animation");
+      // Prevent the default link behavior
+      e.preventDefault();
+      // Provide a short delay to allow the animations to complete
+      setTimeout(() => {
+        window.location.href = link.href;
+      }, 1000);
+    });
+  });
+}
+
 /*=============== INTRO ===============*/
 // Access the intro section
 const intro = document.getElementById("intro");
@@ -12,6 +45,26 @@ if (intro) {
       // Toggle the active class on all links
       intro.querySelector(".link--active").classList.remove("link--active");
       heading.querySelector(".link").classList.add("link--active");
+    });
+  });
+}
+
+/*===== TOGGLE PAGE ANIMATION =====*/
+// Validate whether the intro section exists
+// and the motion preference is not reduced
+if (intro && !prefersReducedMotion) {
+  // Animation before leaving the page
+  intro.querySelectorAll(".intro__heading").forEach((heading) => {
+    // Listen for a mouse click event
+    heading.addEventListener("click", (e) => {
+      // Toggle the animation
+      body.classList.add("unload-animation");
+      // Prevent the default link behavior
+      e.preventDefault();
+      // Provide a short delay to allow the animations to complete
+      setTimeout(() => {
+        window.location.href = heading.querySelector(".link").href;
+      }, 1000);
     });
   });
 }
